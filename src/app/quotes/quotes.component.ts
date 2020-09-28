d9babe156ab207b60a918af9fa60d137df8c9167
Ashisoma <okalionesmus@gmail.com>
2020-09-29 00:30:07 +0300

    add styles to quote form

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,39 +7,38 @@ import { Quote } from '../quote';
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
-export class QuotesComponent implements OnInit
-{
+export class QuotesComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote (1, 'The journey of life may not become easier as we grow older, but we do seem to understand it better as our perspectives evolve.','Drake',new  Date(2019,1,2)),
-    new Quote(1, 'Watch finding Nemo', 'Onesmus Okali',new Date(2020,9,9)),
-    new Quote(3,'Only know you you love her when you let her go.','Martin Chris', new Date(2017,8,17) )
+    new Quote(1, 'The journey of life may not become easier as we grow older, but we do seem to understand it better as our perspectives evolve.', 'Drake', new Date(2019, 1, 2)),
+    new Quote(1, "Your time is limited, so don't waste it living someone else 's life. Don't be trapped by dogma – which is living with the results of other people's thinking." ,'Steve Jobs',  new Date(2020, 9, 9)),
+new Quote(3, 'Only know you you love her when you let her go.', 'Martin Chris', new Date(2017, 8, 17))
   
   ];
-  counted(index){}
-  // counter = 0;
-  // increment(index) {
-  //   t
-  constructor() { }
+counted(index){ }
+// counter = 0;
+// increment(index) {
+//   t
+constructor() { }
 
-  toggleDetails(index){
-    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+toggleDetails(index){
+  this.quotes[index].showDetails = !this.quotes[index].showDetails;
+}
+
+completeQuote(isComplete, index){
+  if (isComplete) {
+    this.quotes.splice(index, 1);
   }
+}
 
-  completeQuote(isComplete, index){
-    if (isComplete) {
-      this.quotes.splice(index,1);
-    }
-  }
+addNewQuote(quote){
+  let quoteLength = this.quotes.length;
+  quote.id = quoteLength + 1;
+  quote.completeDate = new Date(quote.completeDate)
+  this.quotes.push(quote)
 
-  addNewQuote(quote){
-    let quoteLength = this.quotes.length;
-    quote.id = quoteLength +1;
-    quote.completeDate = new Date(quote.completeDate)
-    this.quotes.push(quote)
- 
-   }
+}
 
-  ngOnInit() {
-  }
+ngOnInit() {
+}
 
 }
